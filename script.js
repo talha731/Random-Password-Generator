e98e43aa223226bbceb6106706739f54d9521d2b
talha731 <118377080+talha731@users.noreply.github.com>
2023-01-10 22:54:06 +0000

    done

--- script.js
+++ script.js
@@ -95,40 +95,7 @@ var upperCasedCharacters = [
 //        passLength = prompt("Password must be between 10 and 64")
 //     }
 
-//     let upperCase = prompt("Would you like uppercase characters?");
-//     if (upperCase == 'yes'){
-//        let capitalLetter = getRandom(upperCasedCharacters)
-//     }
-
-
-   
-      // let  lowerCase= "Would you like lowercase characters?";
-      // if (confirm(lowerCase) == true) {
-      //   lowerCase = "Password will contain lowercase characters";
-      // } else {
-      //   lowerCase = "Password will NOT contain lowercase characters";
-      // }
-      
-    
-   
-      // let  specials= "Would you like special characters?.";
-      // if (confirm(specials) == true) {
-      //   specials = "Password will contain special characters";
-      // } else {
-      //   specials = "Password will NOT contain special characters";
-      // }
-     
-    
-   
-      // let numbers = "Would you like numbers characters?.";
-      // if (confirm(numbers) == true) {
-      //   numbers = "Password will contain numbers";
-      // } else {
-      //   numbers = "Password will NOT contain numbers";
-      // }
-      
-// }
-// console.log(generate());
+//  function getPasswordOptions() {
 
 function passWordLength() {
   let passLength = parseInt(prompt("Please Enter length of password"))
@@ -140,12 +107,7 @@ function passWordLength() {
 
 function isCapital() {
   let capital = prompt('Would you like uppercase characters?')
-      
-  // console.log(capital);
-  // while(capital != 'yes' || capital != 'no') {
-  //   capital = prompt('yes or no?')
-  // }
-  
+    
   if (capital == 'yes') {
     return true
   } else if (capital == 'no'){
@@ -179,6 +141,7 @@ function isLowerCase() {
     return false
   } 
 }
+ 
 
 
 
@@ -204,7 +167,7 @@ function generatePassword()
     let char = isSpecialChar()
     let number = isNumber()
     let lower = isLowerCase()
-    console.log(passLength)
+    console.log(passLength);
     for (let i = 0; i < passLength; i++) {
       if(capital){
         password += getRandom(upperCasedCharacters)
@@ -220,12 +183,12 @@ function generatePassword()
       }
       
     }
-    console.log(password)
+   
     return password;
   };
 
 
-console.log(generatePassword());
+ console.log(generatePassword());
 
 
 
@@ -236,10 +199,13 @@ var generateBtn = document.querySelector('#generate');
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-  var passwordText = document.querySelector('#password');
+  var passwordText = document.querySelector('#password')
 
-  passwordText.value = password;
+  
+  
 }
 
+
+
 // Add event listener to generate button
-generateBtn.addEventListener('click', writePassword)
+generateBtn.addEventListener('click', writePassword);
