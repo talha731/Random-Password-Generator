471d5c4a5178881b19d374117b71e10d6ad51380
talha731 <118377080+talha731@users.noreply.github.com>
2023-01-10 17:31:59 +0000

    code not showing on the webpage

--- script.js
+++ script.js
@@ -87,47 +87,98 @@ var upperCasedCharacters = [
   'Y',
   'Z'
 ];
-let characters= [specialCharacters,upperCasedCharacters,lowerCasedCharacters,numericCharacters];
+
 // Function to prompt user for password options
-function generate() {
- 
-    let upperCase = "Would you like uppercase characters?";
-    if (confirm(upperCase) == true) {
-      upperCase = "Password will contain uppercase characters";
-    } else {
-      upperCase = "Password will NOT contain uppercase characters";
-    }
-  
+// function generate() {
+//     let passLength = prompt("Please Enter length of password")
+//     if (passLength <= 10 && passLength >= 65) {
+//        passLength = prompt("Password must be between 10 and 64")
+//     }
+
+//     let upperCase = prompt("Would you like uppercase characters?");
+//     if (upperCase == 'yes'){
+//        let capitalLetter = getRandom(upperCasedCharacters)
+//     }
+
 
    
-      let  lowerCase= "Would you like lowercase characters?";
-      if (confirm(lowerCase) == true) {
-        lowerCase = "Password will contain lowercase characters";
-      } else {
-        lowerCase = "Password will NOT contain lowercase characters";
-      }
+      // let  lowerCase= "Would you like lowercase characters?";
+      // if (confirm(lowerCase) == true) {
+      //   lowerCase = "Password will contain lowercase characters";
+      // } else {
+      //   lowerCase = "Password will NOT contain lowercase characters";
+      // }
       
     
    
-      let  specials= "Would you like special characters?.";
-      if (confirm(specials) == true) {
-        specials = "Password will contain special characters";
-      } else {
-        specials = "Password will NOT contain special characters";
-      }
+      // let  specials= "Would you like special characters?.";
+      // if (confirm(specials) == true) {
+      //   specials = "Password will contain special characters";
+      // } else {
+      //   specials = "Password will NOT contain special characters";
+      // }
      
     
    
-      let numbers = "Would you like numbers characters?.";
-      if (confirm(numbers) == true) {
-        numbers = "Password will contain numbers";
-      } else {
-        numbers = "Password will NOT contain numbers";
-      }
+      // let numbers = "Would you like numbers characters?.";
+      // if (confirm(numbers) == true) {
+      //   numbers = "Password will contain numbers";
+      // } else {
+      //   numbers = "Password will NOT contain numbers";
+      // }
       
-}
-console.log(generate());
+// }
+// console.log(generate());
 
+function passWordLength() {
+  let passLength = parseInt(prompt("Please Enter length of password"))
+  while (passLength <= 10 || passLength >= 65) {
+     passLength = parseInt(prompt("Password must be between 10 and 65"))
+  }
+  return passLength
+}
+
+function isCapital() {
+  let capital = prompt('Would you like uppercase characters?')
+      
+  // console.log(capital);
+  // while(capital != 'yes' || capital != 'no') {
+  //   capital = prompt('yes or no?')
+  // }
+  
+  if (capital == 'yes') {
+    return true
+  } else if (capital == 'no'){
+    return false
+  }
+}
+
+function isSpecialChar() {
+  let char = prompt('Would you like special character?')
+  if (char == 'yes') {
+    return true
+  } else if (char == 'no'){
+    return false
+  } 
+}
+
+function isNumber() {
+  let number = prompt('Would you like numberr?')
+  if (number == 'yes') {
+    return true
+  } else if (number == 'no'){
+    return false
+  } 
+}
+
+function isLowerCase() {
+  let lower = prompt('Would you like lower caser?')
+  if (lower == 'yes') {
+    return true
+  } else if (lower == 'no'){
+    return false
+  } 
+}
 
 
 
@@ -136,30 +187,40 @@ console.log(generate());
 
 // Function for getting a random element from an array
 function getRandom(arr) {
-  
-  
-  
-
-
+  return arr[Math.floor(Math.random() * arr.length)]
 }
-
-
-
-
-
+ 
 
 
 
 
 // Function to generate password with user input
 function generatePassword() 
+  
   {
-    let password = "";
-    for (let i = 0; i < length; i++) {
-      password += characters.charAt(
-        Math.floor(Math.random() * characters.length)
-      );
+    let password = ""
+    let passLength = passWordLength()
+    let capital = isCapital()
+    let char = isSpecialChar()
+    let number = isNumber()
+    let lower = isLowerCase()
+    console.log(passLength)
+    for (let i = 0; i < passLength; i++) {
+      if(capital){
+        password += getRandom(upperCasedCharacters)
+      }
+      if(char){
+        password += getRandom(specialCharacters)
+      }
+      if(number){
+        password += getRandom(numericCharacters)
+      }
+      if(lower){
+        password += getRandom(lowerCasedCharacters)
+      }
+      
     }
+    console.log(password)
     return password;
   };
 
